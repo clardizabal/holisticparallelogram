be217404449c40a03100f8009ae38c3915410231
clardizabal <chris.lardizabal@gmail.com>
2016-04-22 20:10:55 -0700

    add anchor tags to saved places to google maps

--- client/components/SavedPlaceEntry.js
+++ client/components/SavedPlaceEntry.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 var SavedPlaceEntry = (props) => (
   <div className='saved-place-entry animated fadeIn'>
-    <p className='saved-place-name'>{ props.savedPlace.name }</p>
-    <p className='saved-place-address'>{ props.savedPlace.address }</p>
+    <a href={'https://www.google.com/maps/place/' + props.savedPlace.name + ' ' + props.savedPlace.address} target="_blank">
+      <p className='saved-place-name'>{ props.savedPlace.name }</p>
+      <p className='saved-place-address'>{ props.savedPlace.address }</p>
+    </a>
     <div>
       <a className='saved-place-entry-link' href={'//www.images.google.com/search?q=' + props.savedPlace.name + ' ' + props.savedPlace.address + '&tbm=isch'}
       target='_blank'>View Images</a>
